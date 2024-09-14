f24f733776af35867050c62a27a7acd1025f1cf2
Gokulnathz <gokulnaths1992@gmail.com>
2024-09-15 01:53:34 +0530

    onam

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+       <div className="onam-container">
+      <h1 className="onam-heading animate-text">Happy Onam 2024!</h1>
+      <p className="onam-message"><span style={{fontSize:"50px", color:'#e67e22',fontWeight: 'bold'}}>Gokul </span> Wishing you and your family a joyful and prosperous Onam!</p>
+      <img 
+        src="https://media.tenor.com/u95T1-M2AokAAAAj/onam-happy-onam.gif" 
+        alt="Onam Flower" 
+        className="onam-image" 
+      />
+    </div>
+    </>
+  )
+}
+
+export default App
